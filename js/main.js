3c5fe90fb48f3a698f2145f52bafaccb7537dd71
samiul19 <mdfarhansadiq01@gmail.com>
2022-12-02 16:49:49 +0600

    code

--- js/main.js
+++ js/main.js
@@ -440,3 +440,14 @@ $(function() {
 
 });
 
+const filled = document.querySelector('.filled');
+
+function update()
+{
+	filled.style.width = `${((window.scrollY) / (document.body.scrollHeight - window.innerHeight) * 100)}%`;
+
+	requestAnimationFrame(update);
+}
+
+
+update();
